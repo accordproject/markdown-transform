4898f5458ab4999e05254f256178577066d6618c
Jerome Simeon <jeromesimeon@me.com>
2019-09-12 12:35:25 -0400

    fix(concerto) ensure uniform use of the same concerto dependency through the various packages
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/markdown-common/index.js
+++ packages/markdown-common/index.js
@@ -19,6 +19,8 @@
  * @module markdown-transform
  */
 
+module.exports.ComposerConcerto = require('composer-concerto');
+
 module.exports.Logger = require('./lib/Logger');
 module.exports.Stack = require('./lib/Stack');
 module.exports.Models = require('./lib/Models');
