226e8d52ffcb7c9cbf191f8958a74ad3ca6789cd
Dan Selman <danscode@selman.org>
2020-04-15 11:32:07 +0100

    (wip) transform api
    Signed-off-by: Dan Selman <danscode@selman.org>

---
+++ packages/markdown-transform/README.md
@@ -0,0 +1,36 @@
+# Markdown Transform API
+
+High-level API to transform markdown into different formats.
+
+## Installation
+
+```
+npm install -g @accordproject/markdown-transform
+```
+
+## Usage
+
+```
+const transform = require('@accordproject/markdown-transform').transform;
+
+// convert a markdown string to an html string
+//   first argument is the markdown string to be converted
+//   second argument is the source format
+//   third argument is an array of destination formats to pass through
+const htmlString = transform(markdownString, 'markdown', ['html']);
+console.log(htmlString);
+```
+
+## Transformation Graph
+
+You can generate a PlantUML state diagram for the supported transformations using the followning code:
+
+```
+const generateTransformationDiagram = require('@accordproject/markdown-transform').generateTransformationDiagram;
+const plantUMLStateDiagram = generateTransformationDiagram();
+```
+
+## License <a name="license"></a>
+Accord Project source code files are made available under the Apache License, Version 2.0 (Apache-2.0), located in the LICENSE file. Accord Project documentation files are made available under the Creative Commons Attribution 4.0 International License (CC-BY-4.0), available at http://creativecommons.org/licenses/by/4.0/.
+
+© 2017-2019 Clause, Inc.
---
+++ packages/markdown-transform/test/transform.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const chai = require('chai');
+const fs = require('fs');
+const path = require('path');
+chai.use(require('chai-string'));
+
+chai.should();
+chai.use(require('chai-things'));
+chai.use(require('chai-as-promised'));
+
+const transform = require('../lib/transform').transform;
+const generateTransformationDiagram = require('../lib/transform').generateTransformationDiagram;
+const acceptanceMd = fs.readFileSync(path.resolve(__dirname, 'data', 'acceptance.md'), 'utf8');
+const acceptanceJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data', 'acceptance.json'), 'utf8'));
+
+describe('#transform', () => {
+
+    describe('#markdown', () => {
+        it('markdown -> commonmark', () => {
+            const result = transform(acceptanceMd, 'ciceroedit', ['commonmark']);
+            result.$class.should.equal('org.accordproject.commonmark.Document');
+        });
+    });
+
+    describe('#commonmark', () => {
+        it('commonmark -> markdown', () => {
+            const result = transform(acceptanceJson, 'commonmark', ['markdown']);
+            result.should.startWith('Heading');
+        });
+
+        it('commonmark -> ciceromark', () => {
+            const result = transform(acceptanceJson, 'commonmark', ['ciceromark']);
+            result.$class.should.equal('org.accordproject.commonmark.Document');
+        });
+
+        it('commonmark -> plaintext', () => {
+            const result = transform(acceptanceJson, 'commonmark', ['plaintext']);
+            result.should.startWith('Heading');
+        });
+    });
+
+    describe('#ciceroedit', () => {
+        it('ciceroedit -> ciceromark_noquotes', () => {
+            const result = transform(acceptanceMd, 'ciceroedit', ['ciceromark_noquotes']);
+            result.$class.should.equal('org.accordproject.commonmark.Document');
+        });
+    });
+
+    describe('#multi', () => {
+        it('ciceroedit -> ciceromark_noquotes -> slate', () => {
+            const result = transform(acceptanceMd, 'ciceroedit', ['ciceromark_noquotes','slate']);
+            result.document.object.should.equal('document');
+        });
+    });
+
+});
+
+
+describe('#generateTransformationDiagram', () => {
+    it('converts graph to PlantUML diagram', () => {
+        const result = generateTransformationDiagram();
+        result.trim().should.startWith('// Automatically generated. Do not edit!');
+    });
+});
+
