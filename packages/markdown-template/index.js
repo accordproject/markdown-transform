147ee132c8b1fb6ee65668ac12c789a0141bae20
Jerome Simeon <jeromesimeon@me.com>
2020-04-23 10:37:52 -0400

    Add proper index.js
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/markdown-template/index.js
+++ packages/markdown-template/index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,47 +14,11 @@
 
 'use strict';
 
-const Fs = require('fs');
-const Logger = require('@accordproject/concerto-core').Logger;
-const parserOfTemplateAst = require('./lib/FromTemplateAst').parserOfTemplateAst;
+/**
+ * Export the framework and plugins
+ * @module markdown-transform
+ */
 
-require('yargs')
-    .scriptName('markus')
-    .usage('$0 <cmd> [args]')
-    .demandCommand(1, '# Please specify a command')
-    .recommendCommands()
-    .strict()
-    .command('parse', 'parse a string against a template', (yargs) => {
-        yargs.option('input', {
-            describe: 'path to the input',
-            type: 'string'
-        });
-        yargs.option('template', {
-            describe: 'path to the template',
-            type: 'string'
-        });
-    }, (argv) => {
-        if (argv.verbose) {
-            Logger.info(`transform input ${argv.input} file`);
-        }
+module.exports.parserOfTemplateAst = require('./lib/FromTemplateAst').parserOfTemplateAst;
 
-        try {
-            const template = JSON.parse(Fs.readFileSync(argv.template, 'utf8'));
-            const text = Fs.readFileSync(argv.input, 'utf8');
-            const result = parserOfTemplateAst(template).parse(text);
-            if (result.status) {
-                Logger.info('\n'+JSON.stringify(result.value));
-            } else {
-                Logger.error(JSON.stringify(result));
-            }
-        } catch (err){
-            Logger.error(err.message);
-            return;
-        }
-    })
-    .option('verbose', {
-        alias: 'v',
-        default: false
-    })
-    .help()
-    .parse();
+
