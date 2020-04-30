41f402dd29575e3a01698eec5b135169d84a3257
Jerome Simeon <jeromesimeon@me.com>
2020-04-30 12:10:06 -0400

    refactor(test) Reorganize template tests
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/markdown-template/test/TemplateTransformer.js
+++ packages/markdown-template/test/TemplateTransformer.js
@@ -21,20 +21,20 @@ const ModelLoader = require('@accordproject/concerto-core').ModelLoader;
 const TemplateTransformer = require('../lib/TemplateTransformer').TemplateTransformer;
 const normalizeText = require('../lib/TemplateTransformer').normalizeText;
 
-const template4 = JSON.parse(Fs.readFileSync('./test/data/grammar4.json', 'utf8'));
-const text4 = Fs.readFileSync('./test/data/text4.md', 'utf8');
-const text4Err1 = Fs.readFileSync('./test/data/text4Err1.md', 'utf8');
-const text4Err2 = Fs.readFileSync('./test/data/text4Err2.md', 'utf8');
+const grammar2 = JSON.parse(Fs.readFileSync('./test/data/template2/grammar2.json', 'utf8'));
+const sample2 = Fs.readFileSync('./test/data/template2/sample2.md', 'utf8');
+const sample2Err1 = Fs.readFileSync('./test/data/template2/sample2Err1.md', 'utf8');
+const sample2Err2 = Fs.readFileSync('./test/data/template2/sample2Err2.md', 'utf8');
 
-const textPartLarge = normalizeText(Fs.readFileSync('./test/data/large.txt', 'utf8'));
-const textLarge = Fs.readFileSync('./test/data/large.md', 'utf8');
+const samplePartLarge = normalizeText(Fs.readFileSync('./test/data/templateLarge/large.txt', 'utf8'));
+const sampleLarge = Fs.readFileSync('./test/data/templateLarge/large.md', 'utf8');
 const templateLarge = {
     '$class':'org.accordproject.ciceromark.template.Contract',
     'name':'myContract',
     'id':'contract1',
     'type':'org.test.MyContract',
     'nodes': [
-        { '$class': 'org.accordproject.ciceromark.template.Text', 'value': textPartLarge },
+        { '$class': 'org.accordproject.ciceromark.template.Text', 'value': samplePartLarge },
         { '$class': 'org.accordproject.ciceromark.template.Text', 'value': 'This is contract text, followed by a clause:' },
         {
             '$class':'org.accordproject.ciceromark.template.Clause',
@@ -57,37 +57,38 @@ const templateLarge = {
         { '$class': 'org.accordproject.ciceromark.template.Text', 'value': 'There is a penalty of ' },
         { '$class': 'org.accordproject.ciceromark.template.Variable', 'name': 'penalty', 'type': 'Double' },
         { '$class': 'org.accordproject.ciceromark.template.Text', 'value': '% for non compliance.' },
-        { '$class': 'org.accordproject.ciceromark.template.Text', 'value': textPartLarge }
+        { '$class': 'org.accordproject.ciceromark.template.Text', 'value': samplePartLarge }
     ]
 };
 
-const model4 = './test/data/model4.cto';
+const model2 = './test/data/template2/model2.cto';
+const modelLarge = './test/data/templateLarge/modelLarge.cto';
 
 // Tests
 describe('#parse', () => {
-    describe('#template4', () => {
+    describe('#template2', () => {
         it('should parse', async () => {
-            (new TemplateTransformer()).parse(text4,template4).penalty.should.equal(10.99);
+            (new TemplateTransformer()).parse(sample2,grammar2).penalty.should.equal(10.99);
         });
         it('should parse (validate)', async () => {
-            const modelManager = await ModelLoader.loadModelManager(null,[model4]);
-            (new TemplateTransformer()).parse(text4,template4,modelManager).penalty.should.equal(10.99);
+            const modelManager = await ModelLoader.loadModelManager(null,[model2]);
+            (new TemplateTransformer()).parse(sample2,grammar2,modelManager).penalty.should.equal(10.99);
         });
     });
 
-    describe('#template4Err', () => {
+    describe('#template2 (error)', () => {
         it('should fail parsing (extra text)', async () => {
-            (() => (new TemplateTransformer()).parse(text4Err1,template4)).should.throw('Parse error at line 5 column 49\nThere is a penalty of 10.99% for non compliance.X');
+            (() => (new TemplateTransformer()).parse(sample2Err1,grammar2)).should.throw('Parse error at line 5 column 49\nThere is a penalty of 10.99% for non compliance.X');
         });
         it('should fail parsing (wrong text)', async () => {
-            (() => (new TemplateTransformer()).parse(text4Err2,template4)).should.throw('Parse error at line 3 column 77\nThis is a contract between "Steve" and "Betty" for the amount of 3131.00 EUR, even in the presence of forcemajeure.');
+            (() => (new TemplateTransformer()).parse(sample2Err2,grammar2)).should.throw('Parse error at line 3 column 77\nThis is a contract between "Steve" and "Betty" for the amount of 3131.00 EUR, even in the presence of forcemajeure.');
         });
     });
 
     describe('#templateLarge', () => {
         it('should parse', async () => {
-            const modelManager = await ModelLoader.loadModelManager(null,[model4]);
-            (new TemplateTransformer()).parse(textLarge,templateLarge,modelManager).penalty.should.equal(10.99);
+            const modelManager = await ModelLoader.loadModelManager(null,[modelLarge]);
+            (new TemplateTransformer()).parse(sampleLarge,templateLarge,modelManager).penalty.should.equal(10.99);
         });
     });
 });
