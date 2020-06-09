f093dc11e26fa094652e9d7ab12ac17f4de9584f
Jerome Simeon <jeromesimeon@me.com>
2020-06-09 01:45:03 -0400

    WIP Calculates a template grammar for a complex type
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

---
+++ packages/markdown-template/lib/ModelVisitor.js
@@ -0,0 +1,155 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const Field = require('@accordproject/concerto-core').Field;
+const RelationshipDeclaration = require('@accordproject/concerto-core').RelationshipDeclaration;
+const EnumDeclaration = require('@accordproject/concerto-core').EnumDeclaration;
+const EnumValueDeclaration = require('@accordproject/concerto-core').EnumValueDeclaration;
+const ClassDeclaration = require('@accordproject/concerto-core').ClassDeclaration;
+const util = require('util');
+
+const { NS_PREFIX_CommonMarkModel } = require('@accordproject/markdown-common').CommonMarkModel;
+const NS_PREFIX_TemplateMarkModel = require('./externalModels/TemplateMarkModel').NS_PREFIX_TemplateMarkModel;
+
+/**
+ * Converts concerto models to TemplateMark
+ *
+ * @private
+ * @class
+ */
+class ModelVisitor {
+    /**
+     * Visitor design pattern
+     * @param {Object} thing - the object being visited
+     * @param {Object} parameters  - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visit(thing, parameters) {
+        if (thing instanceof EnumDeclaration) {
+            return this.visitEnumDeclaration(thing, parameters);
+        } else if (thing instanceof ClassDeclaration) {
+            return this.visitClassDeclaration(thing, parameters);
+        } else if (thing instanceof Field) {
+            return this.visitField(thing, parameters);
+        } else if (thing instanceof RelationshipDeclaration) {
+            return this.visitRelationshipDeclaration(thing, parameters);
+        } else if (thing instanceof EnumValueDeclaration) {
+            return this.visitEnumValueDeclaration(thing, parameters);
+        } else {
+            throw new Error('Unrecognised type: ' + typeof thing + ', value: ' + util.inspect(thing, { showHidden: true, depth: 1 }));
+        }
+    }
+
+    /**
+     * Visitor design pattern
+     * @param {EnumDeclaration} enumDeclaration - the object being visited
+     * @param {Object} parameters  - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visitEnumDeclaration(enumDeclaration, parameters) {
+        let result = {};
+        result.$class = NS_PREFIX_TemplateMarkModel + 'EnumVariableDefinition';
+        result.name = parameters.type;
+        return result;
+    }
+
+    /**
+     * Visitor design pattern
+     * @param {ClassDeclaration} classDeclaration - the object being visited
+     * @param {Object} parameters - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visitClassDeclaration(classDeclaration, parameters) {
+        let result = {};
+        result.$class = NS_PREFIX_TemplateMarkModel + 'WithDefinition';
+        result.name = parameters.name;
+        result.nodes = [];
+
+        let first = true;
+        classDeclaration.getProperties().forEach((property,index) => {
+            if (!first) {
+                let textNode = {};
+                textNode.$class = NS_PREFIX_CommonMarkModel + 'Text';
+                textNode.text = ' ';
+                result.nodes.push(textNode);
+            }
+            result.nodes.push(property.accept(this, parameters));
+            first = false;
+        });
+
+        return result;
+    }
+
+    /**
+     * Visitor design pattern
+     * @param {Field} field - the object being visited
+     * @param {Object} parameters  - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visitField(field, parameters) {
+        const fieldName = field.getName();
+
+        let result = {};
+        result.$class = NS_PREFIX_TemplateMarkModel + 'VariableDefinition';
+        result.name = fieldName;
+        if(field.isArray()) {
+            const arrayResult = {};
+            arrayResult.$class = NS_PREFIX_TemplateMarkModel + 'JoinDefinition';
+            arrayResult.separator = ' '; // XXX {{#join }}
+            arrayResult.name = fieldName;
+            arrayResult.nodes = [result];
+            result = arrayResult;
+        }
+        if(field.isOptional()) {
+            const optionalResult = {};
+            optionalResult.$class = NS_PREFIX_TemplateMarkModel + 'OptionalDefinition';
+            optionalResult.name = fieldName;
+            optionalResult.whenSome = [result];
+            optionalResult.whenNone = [];
+            result = optionalResult;
+        }
+
+        return result;
+    }
+
+    /**
+     * Visitor design pattern
+     * @param {EnumValueDeclaration} enumValueDeclaration - the object being visited
+     * @param {Object} parameters  - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visitEnumValueDeclaration(enumValueDeclaration, parameters) {
+        throw new Error('visitEnumValueDeclaration not handled');
+    }
+
+    /**
+     * Visitor design pattern
+     * @param {Relationship} relationship - the object being visited
+     * @param {Object} parameters  - the parameter
+     * @return {Object} the result of visiting or null
+     * @private
+     */
+    visitRelationshipDeclaration(relationship, parameters) {
+        throw new Error('visitRelationshipDeclaration');
+    }
+}
+
+module.exports = ModelVisitor;
--- packages/markdown-template/test/data/installment-sale/sample.md
+++ packages/markdown-template/test/data/installment-sale/sample.md
@@ -1,6 +1,6 @@
-"Dan" agrees to pay to "Ned" the total sum 10,000.00 EUR, in the manner following:
+"Dan" agrees to pay to "Ned" the total sum 10000.00 EUR, in the manner following:
 
-500.00 EUR is to be paid at closing, and the remaining balance of 9,500.00 EUR shall be paid as follows:
+500.00 EUR is to be paid at closing, and the remaining balance of 9500.00 EUR shall be paid as follows:
 
 500.00 EUR or more per month on the first day of each and every month, and continuing until the entire balance, including both principal and interest, shall be paid in full -- provided, however, that the entire balance due plus accrued interest and any other amounts due here-under shall be paid in full on or before 24 months.
 
