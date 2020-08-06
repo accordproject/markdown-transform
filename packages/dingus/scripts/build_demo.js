e1d30b2398bbb33b33e3c0d44f95426f2b4ece1b
Jerome Simeon <jeromesimeon@me.com>
2020-08-05 23:57:59 -0400

    feature(dingus) WIP : initial ciceromark dingus
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

---
+++ packages/dingus/scripts/build_demo.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/* eslint-env es6 */
+
+const shell = require('shelljs');
+
+shell.rm('-rf', 'demo');
+shell.mkdir('demo');
+
+shell.exec('scripts/demodata.js > lib/sample.json');
+
+shell.exec('node_modules/.bin/pug lib/index.pug --pretty \
+--obj lib/sample.json \
+--out demo');
+
+shell.exec('node_modules/.bin/stylus -u autoprefixer-stylus \
+< lib/index.styl \
+> demo/index.css');
+
+shell.rm('-rf', 'lib/sample.json');
+
+shell.exec('node_modules/.bin/browserify lib/index.js \
+> demo/index.js');
