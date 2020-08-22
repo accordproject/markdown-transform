b263d41fc7dcf51ee73d03c3fc378d8cfba3e140
Jerome Simeon <jeromesimeon@me.com>
2020-08-22 11:46:20 -0400

    fix(cicero) Bug in unquote for conditional/optional
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/markdown-cicero/lib/UnquoteVariables.js
+++ packages/markdown-cicero/lib/UnquoteVariables.js
@@ -26,8 +26,6 @@ function mapObject(obj, stack) {
     switch (obj.$class) {
     // Strip quotes
     case 'org.accordproject.ciceromark.Formula':
-    case 'org.accordproject.ciceromark.Conditional':
-    case 'org.accordproject.ciceromark.Optional':
     case 'org.accordproject.ciceromark.Variable':
     case 'org.accordproject.ciceromark.FormattedVariable':
         stack.append({
