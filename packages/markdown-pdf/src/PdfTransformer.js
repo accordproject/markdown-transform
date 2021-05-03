d4a9778b24756c68ef99176be3af1e80051b007a
Jerome Simeon <jeromesimeon@me.com>
2021-05-03 19:38:47 -0400

    feature(pdf) Separate pdfmake transform
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/markdown-pdf/src/PdfTransformer.js
+++ packages/markdown-pdf/src/PdfTransformer.js
@@ -24,13 +24,6 @@ const { defaultFonts } = require('./pdfmakeutil');
  * Converts a PDF to CiceroMark DOM
  */
 class PdfTransformer {
-
-    /**
-     * Construct the parser.
-     */
-    constructor() {
-    }
-
     /**
      * Converts an pdf buffer to a CiceroMark DOM
      * @param {Buffer} input - pdf buffer
@@ -49,45 +42,41 @@ class PdfTransformer {
     }
 
     /**
-     * Converts a CiceroMark DOM to a PDF Buffer
+     * Converts a CiceroMark DOM to a pdfmake DOM
      * @param {*} input - CiceroMark DOM (JSON)
      * @param {*} options - the PDF generation options
      * @param {boolean} [options.saveCiceroMark] - whether to save source CiceroMark as a custom property (defaults to true)
      * @param {array} [options.templates] - an array of buffers to be saved into the PDF as custom base64 encoded properties (defaults to null)
-     * @param {*} outputStream - the output stream
+     * @return {*} the pdfmake DOM
      */
-    static async toPdf(input, options = { saveCiceroMark: true }, outputStream) {
+    static async ciceroMarkToPdfMake(input, options = { saveCiceroMark: true }) {
         // The JSON document in pdfmake format
         const dd = await ToPdfMake.CiceroMarkToPdfMake(input, options);
-        // Default fonts are better defined at rendering time
-        dd.defaultStyle = {
-            fontSize: 12,
-            font: 'LiberationSerif',
-            lineHeight: 1.5
-        };
-
-        // The Pdf printer
-        const printer = new PdfPrinter(defaultFonts);
-
-        // Printing to stream
-        const pdfDoc = printer.createPdfKitDocument(dd);
-        pdfDoc.pipe(outputStream);
-        pdfDoc.end();
+        return dd;
     }
 
     /**
-     * Converts a TemplateMark DOM to a PDF Buffer
+     * Converts a TemplateMark DOM to a pdfmake DOM
      * @param {*} input - TemplateMark DOM (JSON)
      * @param {*} options - the PDF generation options
      * @param {boolean} [options.saveCiceroMark] - whether to save source CiceroMark as a custom property (defaults to true)
      * @param {array} [options.templates] - an array of buffers to be saved into the PDF as custom base64 encoded properties (defaults to null)
-     * @param {*} outputStream - the output stream
+     * @return {*} the pdfmake DOM
      */
-    static async templateMarktoPdf(input, options = { saveTemplateMark: true }, outputStream) {
+    static async templateMarktoPdfMake(input, options = { saveTemplateMark: true }) {
         // The JSON document in pdfmake format
         const dd = await ToPdfMake.TemplateMarkToPdfMake(input, options);
+        return dd;
+    }
+
+    /**
+     * Converts a pdfmake DOM to a PDF Buffer
+     * @param {*} input - pdfmake DOM (JSON)
+     * @param {*} outputStream - the output stream
+     */
+    static async pdfMakeToPdf(input, outputStream) {
         // Default fonts are better defined at rendering time
-        dd.defaultStyle = {
+        input.defaultStyle = {
             fontSize: 12,
             font: 'LiberationSerif',
             lineHeight: 1.5
@@ -97,10 +86,11 @@ class PdfTransformer {
         const printer = new PdfPrinter(defaultFonts);
 
         // Printing to stream
-        const pdfDoc = printer.createPdfKitDocument(dd);
+        const pdfDoc = printer.createPdfKitDocument(input);
         pdfDoc.pipe(outputStream);
         pdfDoc.end();
     }
+
 }
 
 module.exports = PdfTransformer;
