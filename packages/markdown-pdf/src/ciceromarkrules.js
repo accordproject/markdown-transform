ffa3d8ae95917b76d8ecdd060d8c07bcd3ce9f0b
Jerome Simeon <jeromesimeon@me.com>
2021-05-08 13:30:23 -0400

    refactor(pdf) Make the *Mark visitors rule-based
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

---
+++ packages/markdown-pdf/src/ciceromarkrules.js
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const {
+    unquoteString,
+} = require('./pdfmakeutil');
+
+const rules = {};
+
+// Inlines
+rules.EnumVariable = (visitor, thing, children, parameters) => {
+    const fixedText = thing.elementType === 'String' || thing.identifiedBy ? unquoteString(thing.value) : thing.value;
+    parameters.result.text = fixedText;
+};
+rules.FormattedVariable = (visitor, thing, children, parameters) => {
+    const fixedText = thing.elementType === 'String' || thing.identifiedBy ? unquoteString(thing.value) : thing.value;
+    parameters.result.text = fixedText;
+};
+rules.Variable = (visitor, thing, children, parameters) => {
+    const fixedText = thing.elementType === 'String' || thing.identifiedBy ? unquoteString(thing.value) : thing.value;
+    parameters.result.text = fixedText;
+};
+rules.Formula = (visitor, thing, children, parameters) => {
+    const fixedText = thing.elementType === 'String' || thing.identifiedBy ? unquoteString(thing.value) : thing.value;
+    parameters.result.text = fixedText;
+};
+rules.Optional = (visitor, thing, children, parameters) => {
+    parameters.result.stack = children;
+};
+rules.Conditional = (visitor, thing, children, parameters) => {
+    parameters.result.stack = children;
+};
+
+// Blocks
+rules.Clause = (visitor, thing, children, parameters) => {
+    parameters.result.stack = children;
+};
+
+module.exports = rules;
