f0b675145a3ca2d90650ed566a4409abf44629b7
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-06-05 14:10:07 +0530

    feat: sets-up class for CiceroMark to OOXML transformer
    Defines constructor
    Defines the function for converting CiceroMark to OOXML
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ packages/markdown-docx/src/CiceroMarkToOOXMLTransformer.js
@@ -0,0 +1,54 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/**
+ * Transforms the ciceromark to OOXML
+ */
+class CiceroMarkToOOXMLTransfomer {
+
+    /**
+     * Defines the different types of nodes for CiceroMark JSON
+     */
+    constructor() {
+        this.definedNodes = {
+            computedVariable: 'org.accordproject.ciceromark.ComputedVariable',
+            heading: 'org.accordproject.commonmark.Heading',
+            item: 'org.accordproject.commonmark.Item',
+            list: 'org.accordproject.commonmark.List',
+            listBlock: 'org.accordproject.ciceromark.ListBlock',
+            paragraph: 'org.accordproject.commonmark.Paragraph',
+            softbreak: 'org.accordproject.commonmark.Softbreak',
+            text: 'org.accordproject.commonmark.Text',
+            variable: 'org.accordproject.ciceromark.Variable',
+            emphasize: 'org.accordproject.commonmark.Emph',
+        };
+    }
+
+    /**
+     * Transforms the given CiceroMark JSON to OOXML
+     *
+     * @param {Object} ciceromark CiceroMark JSON to be converted
+     * @param {Object} counter    Counter for different variables based on node name
+     * @param {string} ooxml      Initial OOXML string
+     * @returns {string} Converted OOXML string i.e. CicecoMark->OOXML
+     */
+    toOOXML(ciceromark, counter, ooxml) {
+        let globalOOXML = ooxml;
+        return globalOOXML;
+    }
+}
+
+module.exports = CiceroMarkToOOXMLTransfomer;
