7f3aea6b8c822d542f22609fc5e7a197c357d5ac
Dan Selman <danscode@selman.org>
2023-01-30 15:36:55 +0000

    doc: add code comments
    Signed-off-by: Dan Selman <danscode@selman.org>

--- packages/markdown-transform/test/scratch.js
+++ packages/markdown-transform/test/scratch.js
@@ -1,3 +1,8 @@
+/**
+ * An example that shows how to dynamically load a text template,
+ * specify the types of the variables for the template via a model,
+ * and then generate an HTML document using the template from JSON data.
+ */
 'use strict';
 
 const { ModelManager } = require('@accordproject/concerto-core');
@@ -6,12 +11,22 @@ const { TemplateMarkTransformer } = require('@accordproject/markdown-template');
 
 const templateMarkTransformer = new TemplateMarkTransformer();
 
+/**
+ * Define the structure of the template. The syntax is called 'Template Mark'
+ * and is extended markdown (commonmark) with embedded JS expressions for formulae
+ * and conditions.
+ */
 const template = `
 Hello {{firstName}}{{#if condition="lastName.startsWith('S')"}}Mister{{else}}Dude{{/if}}!
 
 Thank you for visiting us {{% return now.diff(lastVisit,'day') %}} days ago.
 `;
 
+/**
+ * Define the data model for the template. The model must have a concept with
+ * the @template decorator. The types of properties allow the template to be
+ * type-checked.
+ */
 const model = `namespace test@1.0.0
 
 @template
@@ -25,20 +40,34 @@ concept TemplateData {
 const modelManager = new ModelManager();
 modelManager.addCTOModel(model);
 const options = {};
+
+/**
+ * Parse the Template Mark markdown file to a JSON file
+ */
 const templateJson = templateMarkTransformer.fromMarkdownTemplate( {content: template}, modelManager, 'clause', options);
 console.log('==== TEMPLATE MARK ====');
 console.log(JSON.stringify(templateJson, null, 2));
 
+/**
+ * Define the data we will merge with the template to create a document
+ */
 const data = {
     firstName: 'Dan',
     lastName: 'Jones',
     lastVisit: new Date('2023-01-10')
 };
 
+/**
+ * Merge the template JSON with JSON data to create an instance of the template.
+ * This JSON document is called 'CiceroMark'.
+ */
 const ciceroMarkJson = templateMarkTransformer.instantiateCiceroMark(data, templateJson, modelManager, 'clause', null);
 console.log('==== CICEROMARK MARK ====');
 console.log(JSON.stringify(ciceroMarkJson, null, 2));
 
+/**
+ * Convert the CiceroMark JSON document to HTML
+ */
 const htmlTransformer = new HtmlTransformer();
 const html = htmlTransformer.toHtml(ciceroMarkJson);
 
