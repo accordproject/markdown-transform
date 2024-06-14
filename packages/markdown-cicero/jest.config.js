74d4fa6f7b7a004bedf6252262a0a6322157a443
Ertugrul Karademir <ekarademir@gmail.com>
2024-06-14 13:13:27 +0100

    refactor: revert unnecessary config changes
    Signed-off-by: Ertugrul Karademir <ekarademir@gmail.com>

--- packages/markdown-cicero/jest.config.js
+++ packages/markdown-cicero/jest.config.js
@@ -181,14 +181,12 @@ module.exports = {
     // timers: "real",
 
     // A map from regular expressions to paths to transformers
-    transform: {
-        'node_modules/@accordproject/concerto-util/node_modules/axios/index.js': 'babel-jest'
-    },
+    // transform: {},
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    transformIgnorePatterns: [
-        'node_modules/@accordproject/concerto-util/node_modules/axios/(?!index.js)'
-    ],
+    // transformIgnorePatterns: [
+    //     'node_modules'
+    // ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
