92f2300c9b0e49ab001fbde3299f126e5d9a1511
Muhammed Abdulkadir <40035796+muhabdulkadir@users.noreply.github.com>
2024-11-29 10:53:20 +0000

    chore(*): remove unused deps and update concerto deps (#628)
    * chore(*): remove unused deps and update concerto deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * chore(*): remove unused deps and update concerto deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown cli deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown common deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown docx deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown html deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown it cicero deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown pdf deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    * fix(*): resolve markdown slate deps
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    
    ---------
    
    Signed-off-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>
    Co-authored-by: muhammed-abdulkadir <muhammed.abdulkadir@docusign.com>

--- packages/markdown-cicero/jest.config.js
+++ packages/markdown-cicero/jest.config.js
@@ -90,11 +90,6 @@ module.exports = {
     //   "node"
     // ],
 
-    // A map from regular expressions to module names that allow to stub out resources with a single module
-    moduleNameMapper: {
-        '^axios$': 'axios/dist/axios.js'
-    },
-
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
 
